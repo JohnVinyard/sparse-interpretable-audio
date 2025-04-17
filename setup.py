2cdc128b560a116f3e8e39149cda3e78874fe3c9
John Vinyard <johnwho@gmail.com>
2025-04-17 16:05:57 -0500

    Initial commit. Paper and code structure

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'sparse-interpretable-audio',
+  version = '1.6.3',
+  license='MIT',
+  description = 'Models for extracting sparse, interpretable representations of audio',
+  long_description_content_type = 'text/markdown',
+  author = 'John Vinyard',
+  author_email = 'john.vinyard@gmail.com',
+  url = 'https://github.com/JohnVinyard/sparse-interpretable-audio',
+  keywords = [
+    'machine learning',
+    'sparse',
+    'representation learning'
+  ],
+  install_requires=[
+    'torch',
+    'numpy',
+    'scipy',
+    'librosa'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
